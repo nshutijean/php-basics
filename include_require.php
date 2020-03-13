d8757dc6069ad054564cbb736ff087c0550f9d6f
2nchy <j.nshuti73@gmail.com>
2020-03-13 10:59:49 +0200

    include and require statements

---
+++ include_require.php
@@ -0,0 +1,23 @@
+
+
+<?php
+    /* Comments
+        *You must have 3 lines of comments for every 10 lines of code
+        *The “include” statement issues a warning and continues with the execution if the requested file has not been found
+        *The “include” statement should be in most cases except in situations where without the requested file to be include, the entire script cannot run.
+        *Include_once/require_once is ignored if the requested file has already been included using any of the four statements. 
+    */
+    //Code
+
+    //include and include_once
+    include 'file_name_1';
+    include_once 'file_name_2';
+
+    //require and require_once
+    require 'file_name_3';
+    require_once 'file_name_4';
+
+    //example
+
+
+?>
